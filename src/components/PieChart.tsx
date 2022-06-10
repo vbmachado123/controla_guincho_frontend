76d2a363b9ace7854fdb58530b4f5f79c84bb332
Victor Balbino Machado <vbmachado123@gmail.com>
2022-06-10 10:45:15 -0300

    Continuação Dashboard

--- src/components/PieChart.tsx
+++ src/components/PieChart.tsx
@@ -1,34 +1,39 @@
-import {Pie, PieChart, Cell} from 'recharts';
-
-const data = [
-  { name: "Group A", value: 400 },
-  { name: "Group B", value: 300 },
-  { name: "Group C", value: 300 },
-  { name: "Group D", value: 200 }
-];
-const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
-
+import Chart from "react-apexcharts";
 
 export function PieChartBox() {
+     var options = {
+          series: [44, 55, 41, 17, 15],
+          chart: {
+          type: 'donut',
+        },
+        responsive: [{
+          breakpoint: 480,
+          options: {
+            chart: {
+              width: 200
+            },
+            legend: {
+              position: 'bottom'
+            }
+          }
+        }]
+        };
+
     return (
-        <div className="w-1/2 h-60 px-8 py-4 shadow-xl rounded-2xl bg-white flex flex-row">
+        <div className="my-4 w-auto h-64 px-8 py-4 shadow-xl rounded-2xl bg-white flex flex-row">
            <div className="flex flex-col">
                 <h4 className="font-bold text-slate-800">Conta Corrente</h4>
            </div>
-           <PieChart width={100} height={200} >
-                <Pie   data={data}
-                        cx={120}
-                        cy={200}
-                        innerRadius={60}
-                        outerRadius={80}
-                        fill="#8884d8"
-                        paddingAngle={5}
-                        dataKey="value">
-                            {data.map((entry, index) => (
-                            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-                            ))}
-                        </Pie>
-           </PieChart>
+
+           <div className="">
+  {/* <Chart
+
+              options={options}
+              series={options.series}
+              type="bar"
+              width="500"
+            /> */}
+           </div>
         </div>
     );
 }
--- src/pages/Widget.tsx
+++ src/pages/Widget.tsx
@@ -1,22 +1,35 @@
+import { GiTowTruck } from "react-icons/gi";
+import { MdOutlineCategory, MdSupervisorAccount } from "react-icons/md";
 import { Button } from "../components/Button";
+import { DescriptionCard } from "../components/DescriptionCard";
 import { Header } from "../components/Header";
 import { PieChartBox } from "../components/PieChart";
 
 export function Widget() {
+  let data = new Date();
+    let dataFormatada = ((data.getDate() )) + "/" + ((data.getMonth() + 1)) + "/" + data.getFullYear() + ' - ' + data.getHours() + ':' + data.getUTCMinutes(); 
+  
+
     return(
-       <div className="flex flex-col w-screen h-screen scroll-smooth relative overflow-y-auto">
+       <div className="flex flex-col w-screen h-screen scroll-smooth relative bg-slate-100">
           
-            <div className="col-span-6 items-center px-24 justify-center w-full h-full mt-24">
+            <div className="col-span-6 items-center px-24 justify-center w-full h-full mt-24 overflow-y-scroll">
                         <div className="relative flex flex-row justify-end"> 
                             <Button onClick={() => {}} label={"Exportar"} style={"bg-green-500 text-white"} /> 
                             {/* <Button onClick={() => {} } label={"Filtrar"} style={"bg-green-500 text-white"}/>  */}
                         </div>
 
-                        <div className="mt-8 overflow-y-scroll grid grid-flow-col">
+                        <p className="text-red-500">As informações estão estáticas por enquanto</p>
+                       <div className="w-full grid grid-cols-3 justify-between mt-16">
+                           <DescriptionCard title={"Atendimentos"} value={"22"} style={"bg-red-900"} icon={ <MdSupervisorAccount size={96} color='white' opacity={.25} />} description={`Última atualização em: ${dataFormatada}`}/>
+                           <DescriptionCard title={"Veículos"} value={"4"} style={"bg-green-600"} icon={ <GiTowTruck size={96} color='white' opacity={.25} />} description={"Todos os veículos estão em funcionamento"}/>
+                           <DescriptionCard title={"Profissionais"} value={"2"} style={"bg-blue-900"} icon={<MdOutlineCategory size={96} color='white' opacity={.25}/>} description={"Todos os profissionais estão em atuação"}/>
+                       </div>
+
+                        <div className="mt-8 grid grid-col-2">
                             <PieChartBox/>
-                           
+                            <PieChartBox/>
                         </div>
-                       
             </div>
                 <Header currentPage={"home"}/>
         </div>
