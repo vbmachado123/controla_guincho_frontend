e677943c1a11a6ac5fae196bb2d23897ea8766a7
Victor Balbino Machado <vbmachado123@gmail.com>
2022-06-20 07:47:25 -0300

    Tela de Login

---
+++ src/model/ItemList.ts
@@ -0,0 +1,16 @@
+export class ItemList {
+    id: number;
+    name: string;
+    description: string;
+
+    constructor(
+        id: number,
+        name: string,
+        description: string,
+
+    ) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+}
