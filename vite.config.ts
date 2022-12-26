f0c5de7fee8e1a4da5467520eb7239673f53cd05
Hahuma <firminopetterson@gmail.com>
2022-12-26 19:39:06 -0300

    adiciona outDir para a pasta build

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import * as path from 'path'
+import { resolve } from 'path'
+
+const outDir = resolve(__dirname, 'build')
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
+  build: {
+    outDir
+  },
   define: {
     'process.env': {}
   },
