f5b89951510de2a08d7fddd30e437daf3cc445eb
giulliano <giullianomendes033@gmail.com>
2023-01-27 16:30:04 -0300

    url da api alterado para o servidor

--- src/https-commons.ts
+++ src/https-commons.ts
@@ -9,9 +9,9 @@ axios.defaults.headers.post['X-Requested-With'] = 'XMLHttpRequest';
 
 export default axios.create({
     // baseURL: 'http://192.168.0.10:8081/api/v1',
-     baseURL: 'http://localhost:8081/api/v1',
+     //baseURL: 'http://localhost:8081/api/v1',
     // baseURL: 'https://cors-anywhere.herokuapp.com/http://192.168.0.10:8081/api/v1',
-    //baseURL: 'http://188.34.166.175:8081/api/v1',
+    baseURL: 'http://188.34.166.175:8081/api/v1',
 
     headers: {
         "Content-type": "application/json",
